f0b3488012d611f295ddb8f0a2288fe21aa00c92
ivangrinko-dev <grinko.work.1982@mail.ru>
2023-04-26 15:47:30 +0300

    tasks node.js

--- service/service.js
+++ service/service.js
@@ -5,7 +5,7 @@ function getAll() {
   const arr = JSON.parse(fs.readFileSync(path));
   if (!arr.length) throw new Error(`Массив пуст`);
   return arr;
-}
+};
 
 function createEnviroment(label, category, priority) {
   const arr = JSON.parse(fs.readFileSync(path));
@@ -17,15 +17,7 @@ function createEnviroment(label, category, priority) {
   });
   fs.writeFileSync(path, JSON.stringify(arr));
   return arr;
-}
-
-function deleteEnviroment(id) {
-  const arr = JSON.parse(fs.readFileSync(path));
-  const filtered = arr.filter((elem) => elem.id != id);
-
-  fs.writeFileSync(path, JSON.stringify(filtered));
-  return filtered;
-}
+};
 
 function putEnviroment(id, label, category, priority) {
   const arr = JSON.parse(fs.readFileSync(path));
@@ -37,9 +29,19 @@ function putEnviroment(id, label, category, priority) {
       category: category,
       priority: priority,
     });
+  } else {
+    throw new Error("такого id нет");
   }
   fs.writeFileSync(path, JSON.stringify(filtered));
   return filtered;
-}
+};
+
+function deleteEnviroment(id) {
+  const arr = JSON.parse(fs.readFileSync(path));
+  const filtered = arr.filter((elem) => elem.id != id);
+
+  fs.writeFileSync(path, JSON.stringify(filtered));
+  return filtered;
+};
 
 module.exports = { getAll, createEnviroment, deleteEnviroment, putEnviroment };
